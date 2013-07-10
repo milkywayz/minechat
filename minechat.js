bf17ead811a01100c0bbe49699f3717331d90c9b
Nick Porillo <milkywayz@mail.com>
2013-07-09 20:43:01 -0400

    Commands, use mc-protocol instead of mineflayer, 1.6 chat support, and colored chat support

--- minechat.js
+++ minechat.js
@@ -1,5 +1,29 @@
 var readline = require('readline');
-var mineflayer = require('mineflayer');
+var color = require("ansi-color").set;
+var mc = require('minecraft-protocol');
+var c = new Buffer("§", "utf-8")
+
+var colors = new Array();
+colors[c.toString('utf-8') + '0'] = 'black+white_bg';
+colors[c.toString('utf-8') + '1'] = 'white+blue_bg';
+colors[c.toString('utf-8') + '2'] = 'green';
+colors[c.toString('utf-8') + '3'] = 'blue';
+colors[c.toString('utf-8') + '4'] = 'red';
+colors[c.toString('utf-8') + '5'] = 'magenta';
+colors[c.toString('utf-8') + '6'] = 'yellow';
+colors[c.toString('utf-8') + '7'] = 'white';
+colors[c.toString('utf-8') + '8'] = 'white';
+colors[c.toString('utf-8') + '9'] = 'blue';
+colors[c.toString('utf-8') + 'a'] = 'green';
+colors[c.toString('utf-8') + 'b'] = 'cyan';
+colors[c.toString('utf-8') + 'c'] = 'red';
+colors[c.toString('utf-8') + 'd'] = 'magenta';
+colors[c.toString('utf-8') + 'e'] = 'yellow';
+colors[c.toString('utf-8') + 'f'] = 'white';
+colors[c.toString('utf-8') + 'k'] = 'blink';
+colors[c.toString('utf-8') + 'l'] = 'bold';
+colors[c.toString('utf-8') + 'n'] = 'underline';
+colors[c.toString('utf-8') + 'r'] = 'white';
 
 var rl = readline.createInterface({
 	input: process.stdin,
@@ -38,17 +62,55 @@ console.log("connecting to " + host + ":" + port);
 console.log("user: " + user);
 console.log("passwd: " + Array(passwd.length).join('*'));
 
-var bot = mineflayer.createBot({
+var client = mc.createClient({
 	host: host,
 	port: port,
 	username: user,
 	password: passwd
 });
 
-rl.on('line', function(line) {
-	bot.chat(line);
+client.on(0xff, function(packet) {
+    console.info(color('Kicked for ' + packet.reason, "blink+red"));
+    process.exit(1);
+  });
+
+client.on('connect', function() {
+  console.info(color('Successfully connected to ' + host + ':' + port, "blink+green"));
 });
 
-bot.on('chat', function(username, message) {
-	console.log(username + ": " + message);
+rl.on('line', function(line) {  
+	if(line == '') {
+		return;
+	} else if(line == '/quit') {
+		var reason = 'disconnect.quitting';
+		console.info('Disconnected from ' + host + ':' + port);
+		client.write(0xff, { reason: reason });		
+		return;
+	} else if(line == '/end') {
+		console.info('Forcibly ended client');
+		process.exit(0);
+		return;
+	} else if(line == '/reconnect') {
+		client = mc.createClient({
+			host: host,
+			port: port,
+			username: user,
+			password: passwd
+			});
+		return;
+	} 
+	client.write(0x03, {message: line});
+});
+
+client.on(0x03, function(packet) {
+  var j = JSON.parse(packet.message);
+  var chat = "";
+  var arr = j.text.split(c.toString('utf-8'));
+  for(var x = 0; x < arr.length; x++) {
+      var n = arr[x];
+      var m = n.substring(0,1);
+      var p = n.substring(1,n.length);
+	  chat = chat + color(p, colors[c.toString('utf-8') + m]);
+  }
+  console.info(chat);
 });
